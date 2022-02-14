523894e3203329f4fd530ade927584439d88074f
ah_abdelhak <ah_abdelhak@yahoo.com>
2022-02-15 00:49:51 +0200

    Testing Fragments with Dagger-Hilt

---
+++ app/src/debug/java/com/example/androidtestingguide/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.example.androidtestingguide
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
